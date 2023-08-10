cf731a8aae8bf0f284024376342ac7eb4726db9a
Gustavo da Silva Rosa <65796235+guhdsrosa@users.noreply.github.com>
2023-08-10 20:04:04 -0300

    Update index.js

--- src/screens/Store/components/sobre/index.js
+++ src/screens/Store/components/sobre/index.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 const Sobre = () => {
     return(
         <View>
-            <Text>dadsa</Text>
+            <Text>Sobre</Text>
         </View>
     )
 }
